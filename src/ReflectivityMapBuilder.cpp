583951700d6f6b451c516590e14db25aaa54525c
miyalab <miyalab.workspace@gmail.com>
2022-11-08 17:35:35 +0900

    update

---
+++ src/ReflectivityMapBuilder.cpp
@@ -0,0 +1,32 @@
+//-----------------------------
+// include
+//-----------------------------
+// STL
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <filesystem>
+
+// OpenCV
+#include <opencv2/opencv.hpp>
+
+// ROS2
+#include <sensor_msgs/msg/point_cloud.hpp>
+
+using sensor_msgs::msg::PointCloud;
+
+/**
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char **argv)
+{
+    
+}
+
+//-----------------------------------------------------------------------------------
+// end of file
+//-----------------------------------------------------------------------------------
